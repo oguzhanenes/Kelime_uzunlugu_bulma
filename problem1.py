8b04577217e2fa717e85a6363979c30aefabaa25
oguzhan <johndoe@example.com>
2023-09-16 19:20:53 +0300

    islemi cozecek fonksiyon eklendi

--- problem1.py
+++ problem1.py
@@ -1,2 +1,7 @@
-print("merhaba")
+def parametre(metin):
+    cümle = metin.split()
+    uzunluk= len(cümle[-1])
+    print("son kelimenin harf sayısı:",uzunluk)
 
+parametre("bugun hava cok sogumus")
+parametre("bugun hava cok sogukmus")
